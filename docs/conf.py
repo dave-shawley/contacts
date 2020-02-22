dc72bb05c1541ff3b8b2afa3102b065c899af1a4
Dave Shawley <daveshawley@gmail.com>
2020-02-22 10:29:14 -0500

    Add documentation.

---
+++ docs/conf.py
@@ -0,0 +1,10 @@
+import contacts
+
+project = 'contacts'
+author = 'Dave Shawley'
+copyright = '2020, Dave Shawley'
+version = contacts.version
+needs_sphinx = '2.0'
+extensions = []
+template = 'alabaster'
+html_static_path = ['.']
